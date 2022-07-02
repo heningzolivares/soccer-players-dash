538409953730b57523064cc644607dde97fd79c8
Heningz Olivares <heningzolivares.dev@gmail.com>
2022-07-02 19:07:33 -0400

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <Container>
+      <Box>
+        <Typography>Hello world</Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default Home;
